e15384573207036ef6388ca043330b3b4f5812a4
Zyntex <komocsinlaci@gmail.com>
2023-12-09 01:36:26 +0100

    add source style movement

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lcbhop
 Bhop mod featuring *real* **Source** movement in Lethal Company.
+
+Movement code based on [quake3-movement-unity3d](https://github.com/WiggleWizard/quake3-movement-unity3d/tree/master)
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,14 +1,40 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 
-namespace lcbhop
-{
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace lcbhop {
+    [HarmonyPatch( typeof( CharacterController ), "Move" )]
+    class CharacterController_Patch {
+        [HarmonyPrefix]
+        internal static bool Prefix( ref Vector3 motion ) {
+            return !Plugin.patchMove;
+        }
+    }
+
+    [BepInPlugin( MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION )]
+    public class Plugin : BaseUnityPlugin {
+        public static ManualLogSource logger;
+
+        public static bool patchMove = true;
+
+        readonly Harmony harmony = new Harmony( MyPluginInfo.PLUGIN_GUID );
+
+        void Awake( ) {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo( $"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!" );
+
+            logger = Logger;
+
+            harmony.PatchAll( );
+        }
+
+        void OnDestroy( ) {
+            GameObject gameObject = new GameObject( "ComponentAdder" );
+            UnityEngine.Object.DontDestroyOnLoad( gameObject );
+            gameObject.AddComponent<ComponentAdder>( );
         }
     }
 }
